c4c21bb8ca7bfc1b62ccbb64256bc5f9a48aa17f
TheJouster <tomilovdima999@gmail.com>
2024-11-10 20:30:56 +0500

    Task3

---
+++ Tyuiu.TomilovDA.Sprint4.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.TomilovDA.Sprint4.Task3.V3.Lib;
+
+namespace Tyuiu.TomilovDA.Sprint4.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int[,] array = { {9, 4, 5, 4, 8},
+                            {7, 6, 7, 7, 4 },
+                            {4, 4, 3, 5, 6 },
+                            {6, 5, 9, 4, 9 },
+                            {9, 7, 8, 7, 7 }};
+           Assert.AreEqual(14, dataService.Calculate(array));
+        }
+    }
+}
---
+++ Tyuiu.TomilovDA.Sprint4.Task3.V3/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.TomilovDA.Sprint4.Task3.V3.Lib;
+namespace Tyuiu.TomilovDA.Sprint4.Task3.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Двумерные массивы. (статический ввод)                                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                            *");
+            Console.WriteLine("* Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статичес*");
+            Console.WriteLine("*скими значениями в диапазоне от 3 до 9. Подсчитайте количество нечетных э*");
+            Console.WriteLine("*элементов во всем массиве.                                               *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] array = { {9, 4, 5, 4, 8},
+                            {7, 6, 7, 7, 4 },
+                            {4, 4, 3, 5, 6 },
+                            {6, 5, 9, 4, 9 },
+                            {9, 7, 8, 7, 7 }};
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                    
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(array);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
