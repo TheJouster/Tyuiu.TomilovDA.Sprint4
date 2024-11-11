06b9f7dd7434155668afc18725c4e6373f73d473
TheJouster <tomilovdima999@gmail.com>
2024-11-11 23:21:47 +0500

    Task5

---
+++ Tyuiu.TomilovDA.Sprint4.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.TomilovDA.Sprint4.Task5.V13.Lib;
+
+namespace Tyuiu.TomilovDA.Sprint4.Task5.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int[,] matrix = {
+                {7, 9, 7, -6, 7},
+                {9, 9, -8, -6, 7},
+                {-8, -6, 5, -6, 7},
+                {9, 9, 7, -8, 7},
+                {5, 9, 9, -8, 9}};
+            int[,] matrix1 = {
+                {7, 9, 7, 0, 7},
+                {9, 9, 0, 0, 7},
+                {0, 0, 5, 0, 7},
+                {9, 9, 7, 0, 7},
+                {5, 9, 9, 0, 9}};
+            CollectionAssert.AreEqual(matrix1, dataService.Calculate(matrix));
+        }
+    }
+}
---
+++ Tyuiu.TomilovDA.Sprint4.Task5.V13/Program.cs
@@ -0,0 +1,72 @@
+﻿using Tyuiu.TomilovDA.Sprint4.Task5.V13.Lib;
+namespace Tyuiu.TomilovDA.Sprint4.Task5.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.Title = "Спринт #4 | Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Двумерные массивы. (ввод с клавиатуры)                                  *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -2 до 5. Заменить отрицательные    *");
+            Console.WriteLine("* элементы на 0.                                                          *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+
+            for (int i1 = 0; i1 < matrix.GetLength(0); i1++)
+            {
+                for (int j1 = 0; j1 < matrix.GetLength(1); j1++)
+                {
+                    matrix[i1, j1] = random.Next(-2,5);
+                }
+            }
+
+            Console.WriteLine("Заданная матрица:");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(matrix);
+
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write($"{result[i, j]} \t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
