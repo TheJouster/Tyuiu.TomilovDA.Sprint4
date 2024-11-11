a5955e190705ddec0d93815312b0d95b068d9d3d
TheJouster <tomilovdima999@gmail.com>
2024-11-11 22:08:55 +0500

    Task4

---
+++ Tyuiu.TomilovDA.Sprint4.Task4.V24/Program.cs
@@ -0,0 +1,71 @@
+﻿using Tyuiu.TomilovDA.Sprint4.Task4.V24.Lib;
+namespace Tyuiu.TomilovDA.Sprint4.Task4.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Двумерные массивы. (ввод с клавиатуры)                                  *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан двумерный целочисленный массив 5 на 5 элементов, заполненный значения*");
+            Console.WriteLine("*ями с клавиатуры в диапазоне от 5 до 9. Заменить четные элементы массива *");
+            Console.WriteLine("* на 1.                                                                   *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5,5];
+
+            
+            for (int i1 = 0; i1 < matrix.GetLength(0); i1++)
+            {
+                for (int j1 = 0; j1 < matrix.GetLength(1); j1++)
+                {
+                    Console.WriteLine($"Введите {i1},{j1} элемент матрицы");
+                    matrix[i1,j1] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Заданная матрица:");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(matrix);
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write($"{result[i, j]} \t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
